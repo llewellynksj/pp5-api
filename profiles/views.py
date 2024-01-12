5cfa52152a726c242882c83893d8d58cb80438c9
Kate Llewellyn <llewellyn.ksj@gmail.com>
2024-01-12 11:34:28 +0000

    Use custom permissions in profiledetail view

--- profiles/views.py
+++ profiles/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import Profile
 from .serializers import ProfileSerializer
+from pp5_api.permissions import IsOwnerOrReadOnly
 
 
 class ProfileList(APIView):
@@ -15,9 +16,12 @@ class ProfileList(APIView):
 
 class ProfileDetail(APIView):
   serializer_class = ProfileSerializer
+  permission_classes = [IsOwnerOrReadOnly]
+
   def get_object(self, pk):
     try:
       profile = Profile.objects.get(pk=pk)
+      self.check_object_permissions(self.request, profile)
       return profile
     except Profile.DoesNotExist:
       raise Http404
