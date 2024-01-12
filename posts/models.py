806828d8b6be2e2bbd7542a15ac84750171fc76e
Kate Llewellyn <llewellyn.ksj@gmail.com>
2024-01-12 12:03:24 +0000

    Add image_tags field to model, and add image validation

--- posts/models.py
+++ posts/models.py
@@ -6,6 +6,21 @@ class Post(models.Model):
     """
     Post model
     """
+    TAG_CHOICES = [
+      ('tribal', 'Tribal'),
+      ('geometric', 'Geometric'),
+      ('realism', 'Realism'),
+      ('portraits', 'Portraits'),
+      ('illustrative', 'Illustrative'),
+      ('dotwork', 'Dotwork'),
+      ('watercolour', 'Watercolour'),
+      ('neo-traditional', 'Neo-traditional'),
+      ('abstract', 'Abstract'),
+      ('animals', 'Animals'),
+      ('lettering', 'Lettering'),
+      ('traditional', 'Traditional')
+    ]
+
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -14,6 +29,9 @@ class Post(models.Model):
     image = models.ImageField(
         upload_to='images/', default='../default_post_rgq6aq', blank=True
     )
+    image_tags = models.CharField(max_length=50, choices=TAG_CHOICES, blank=True, null=True)
+
+
 
     class Meta:
         ordering = ['-created_at']
--- posts/serializers.py
+++ posts/serializers.py
@@ -8,6 +8,21 @@ class PostSerializer(serializers.ModelSerializer):
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
     is_owner = serializers.SerializerMethodField()
 
+    def validate_image(self, value):
+      if value.size > 1024 * 1024 * 2:
+        raise serializers.ValidationError(
+          'Image size should be less than 2MB'
+        )
+      if value.image.width > 4096:
+        raise serializers.ValidationError(
+          'Image width should be less than 4096px'
+        )
+      if value.image.height > 4096:
+        raise serializers.ValidationError(
+          'Image height should be less than 4096px'
+        )
+      return value
+
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
@@ -17,5 +32,5 @@ class PostSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'owner', 'is_owner', 'profile_id',
             'profile_image', 'created_at', 'updated_at',
-            'title', 'content', 'image'
+            'title', 'content', 'image', 'image_tags'
         ]
