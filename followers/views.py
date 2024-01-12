9da2a3e5a6dfc5acf3db8b17065a8470e32827f6
Kate Llewellyn <llewellyn.ksj@gmail.com>
2024-01-12 14:05:24 +0000

    Add follower views

--- followers/views.py
+++ followers/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from pp5_api.permissions import IsOwnerOrReadOnly
+from .models import Follower
+from .serializers import FollowerSerializer
 
-# Create your views here.
+
+class FollowerList(generics.ListCreateAPIView):
+    """
+    List view allows users to see all following instances
+    Also allows a logged in user to create a following instance
+    associated with their account
+    """
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Follower.objects.all()
+    serializer_class = FollowerSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class FollowerDetail(generics.RetrieveDestroyAPIView):
+    """
+    Detail view allows users to view followers
+    If logged in user can delete their own following instances
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Follower.objects.all()
+    serializer_class = FollowerSerializer
