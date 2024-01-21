a9648b90eb9a89a53be9693f001d3b41d049d995
Kate Llewellyn <llewellyn.ksj@gmail.com>
2024-01-21 10:43:28 +0000

    Add missing import in profiles views.py

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,6 @@
 from rest_framework import generics, filters, status
 from rest_framework.views import APIView
+from rest_framework.response import Response
 from django.db.models import Count
 from django_filters.rest_framework import DjangoFilterBackend
 from pp5_api.permissions import IsOwnerOrReadOnly
