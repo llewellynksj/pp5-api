c7667708f187ffd32bef2d6be487ee3c9a71829d
Kate Llewellyn <llewellyn.ksj@gmail.com>
2024-01-26 15:28:47 +0000

    Amend typo in profile status tags

--- profiles/models.py
+++ profiles/models.py
@@ -5,7 +5,7 @@ from django.db.models.signals import post_save
 STATUS_CHOICES = [
   ('ink slinger', 'Ink Slinger'),
   ('ink addict', 'Ink Addict'),
-  ('loooking for inspo', 'Looking for Inspo'),
+  ('looking for inspo', 'Looking for Inspo'),
 ]
 
 class Profile(models.Model):
